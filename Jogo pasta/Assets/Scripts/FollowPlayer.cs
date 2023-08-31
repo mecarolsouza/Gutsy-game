88d70dbf31b6701371f3178e533bc14193c4e4e7
ayannamcr <ayannamcribeiro@gmail.com>
2023-08-31 10:13:49 -0300

    n

--- Jogo pasta/Assets/Scripts/FollowPlayer.cs
+++ Jogo pasta/Assets/Scripts/FollowPlayer.cs
@@ -10,7 +10,7 @@ public class FollowPlayer : MonoBehaviour
     private void FixedUpdate()
     {
         Vector3 newPosition = player.position + new Vector3 (0,0,-10);
-        newPosition.y = -0.8f;
+        newPosition.y = -2.1f;
         transform.position = newPosition;
 
         transform.position = new Vector3(Mathf.Clamp(transform.position.x,minX,maxX), transform.position.y, transform.position.z);
