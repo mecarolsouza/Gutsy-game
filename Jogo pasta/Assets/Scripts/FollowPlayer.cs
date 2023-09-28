e2a281f6b2db5013251c31d0b08c9c0893a7908e
vitoriaf45 <vitoriafarias081@gmail.com>
2023-09-27 22:40:19 -0300

    f

--- Jogo pasta/Assets/Scripts/FollowPlayer.cs
+++ Jogo pasta/Assets/Scripts/FollowPlayer.cs
@@ -1,65 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    //private Vector3 offset new Vector3(0f, 0f, -10f);
-    [SerializeField] private Transform player;
-    public float smoothSpeed = 0.25f;
-    
-    public float minX, maxX;
-    public float minY, maxY;
-    
-    void Start()
+    public Transform target; // Referência ao transform do jogador que a câmera deve seguir
+    public float smoothSpeed = 0.125f; // Velocidade de interpolação suave
+
+    private Vector3 offset; // O deslocamento inicial entre a câmera e o jogador
+
+    private void Start()
     {
-       // Verifique se o jogador está atribuído.
-        /*if (player != null)
-        {
-            // Defina a posição inicial da câmera para a posição inicial do jogador.
-            Vector3 initialPosition = player.position + new Vector3(0, 0, -10);
-            initialPosition.y = Mathf.Clamp(initialPosition.y, minY, maxY);
-            transform.position = initialPosition;
-        }
-        else
-        {
-            Debug.LogError("O jogador não está atribuído ao script da câmera.");
-        }*/
+        offset = transform.position - target.position; // Calcula o deslocamento inicial
     }
 
-    /*void LateUpdate()
+    private void LateUpdate()
     {
-        // Verifique se o personagem alvo existe.
-        if (player != null)
-        {
-            // Calcule a posição desejada da câmera com base na posição do personagem e no offset.
-            Vector3 newPosition = player.position;
-
-            // Aplique limites X e Y à posição desejada.
-            newPosition.x = Mathf.Clamp(newPosition.x, minX, maxX);
-            newPosition.y = Mathf.Clamp(newPosition.y, minY, maxY);
-
-            // Interpole suavemente a posição atual da câmera em direção à posição desejada.
-            Vector3 smoothedPosition = Vector3.Lerp(transform.position, newPosition, smoothSpeed * Time.deltaTime);
-            transform.position = smoothedPosition;
-        }
-    }*/
-
-    private void FixedUpdate()
-    {
-        Vector3 newPosition = player.position + new Vector3 (0,0,-10);
-        //newPosition.y = Mathf.Clamp(newPosition.y, minY, maxY);
-        //transform.position = newPosition;
-
-        newPosition.y = Mathf.Clamp(newPosition.y, minY, maxY);
-
-        newPosition.x = Mathf.Clamp(newPosition.x, minX, maxX);
-
-        transform.position = newPosition;
-
-        
-        //transform.position = new Vector3(Mathf.Clamp(transform.position.x,minX,maxX), Mathf.Clamp(transform.position.y,minY,maxX), transform.position.z);
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
     }
 
-
+    
 }
+
